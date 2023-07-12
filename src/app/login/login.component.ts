295ca0d3e3531007b4cd54889e5d63f2d3d61ca4
mechanist1 <mlaouhiilyess@gmail.com>
2023-07-12 11:34:23 +0100

    first commit , did the signup/signin

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private router: Router, private http: HttpClient) { }
+
+ngOnInit(){};
+
+}
---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+
+email:String="";
+username:String="";
+password:String="";
+
+constructor(private router: Router, private http: HttpClient) { }
+ngOnInit(){};
+
+}
